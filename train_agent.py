c27d56b520cd9c95f4312dc8c03ab843b264575e
Michael Skinnider <michaelskinnider@gmail.com>
2020-07-04 09:09:45 -0700

    sample from Agent during training

--- train_agent.py
+++ train_agent.py
@@ -3,11 +3,10 @@
 import argparse
 
 import torch
-import pickle
 import numpy as np
 import time
 import os
-from shutil import copyfile
+import random
 
 from model import RNN
 from data_structs import Vocabulary, Experience
@@ -15,28 +14,37 @@ from scoring_functions import get_scoring_function
 from utils import Variable, seq_to_smiles, fraction_valid_smiles, unique
 from vizard_logger import VizardLog
 
+from logging_functions import sample_smiles
+
 # CLI
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=str)
+parser.add_argument('--prior_dir', type=str)
 parser.add_argument('--output_dir', type=str)
 parser.add_argument('--sample_size', type=int, default=10000)
-parser.add_argument('--patience', type=int, default=10000)
+parser.add_argument('--n_steps', type=int, default=5000)
 parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--scoring_function', type=str, default='activity_model',
+                    choices=['activity_model', 'tanimoto', 'no_sulphur'])
+parser.add_argument('--clf_file', type=str)
 args = parser.parse_args()
 
-def train_agent(restore_prior_from='data/Prior.ckpt',
-                restore_agent_from='data/Prior.ckpt',
-                scoring_function='tanimoto',
-                scoring_function_kwargs=None,
-                save_dir=None, learning_rate=0.0005,
-                batch_size=64, n_steps=3000,
-                num_processes=0, sigma=60,
-                experience_replay=0,
-                # new parameters
-                
-                ):
-
-    voc = Vocabulary(init_from_file="data/Voc")
+def train_agent(scoring_function_kwargs=None,
+                save_dir=None, 
+                learning_rate=0.0005,
+                batch_size=64,
+                num_processes=0, ## single process
+                sigma=60,
+                experience_replay=0):
+    ## seed all RNGs for reproducible output
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    if torch.cuda.is_available(): 
+        torch.cuda.manual_seed_all(args.seed)
+    
+    voc_file = os.path.join(args.input_dir, 'Voc')
+    voc = Vocabulary(init_from_file=voc_file)
 
     start_time = time.time()
 
@@ -48,12 +56,15 @@ def train_agent(restore_prior_from='data/Prior.ckpt',
     # By default restore Agent to same model as Prior, but can restore from already trained Agent too.
     # Saved models are partially on the GPU, but if we dont have cuda enabled we can remap these
     # to the CPU.
+    prior_file = os.path.join(args.prior_dir, 'Prior.ckpt')
     if torch.cuda.is_available():
-        Prior.rnn.load_state_dict(torch.load('data/Prior.ckpt'))
-        Agent.rnn.load_state_dict(torch.load(restore_agent_from))
+        Prior.rnn.load_state_dict(torch.load(prior_file))
+        Agent.rnn.load_state_dict(torch.load(prior_file))
     else:
-        Prior.rnn.load_state_dict(torch.load('data/Prior.ckpt', map_location=lambda storage, loc: storage))
-        Agent.rnn.load_state_dict(torch.load(restore_agent_from, map_location=lambda storage, loc: storage))
+        Prior.rnn.load_state_dict(torch.load(prior_file, map_location=lambda \
+                                             storage, loc: storage))
+        Agent.rnn.load_state_dict(torch.load(prior_file, map_location=lambda \
+                                             storage, loc: storage))
 
     # We dont need gradients with respect to Prior
     for param in Prior.rnn.parameters():
@@ -62,14 +73,18 @@ def train_agent(restore_prior_from='data/Prior.ckpt',
     optimizer = torch.optim.Adam(Agent.rnn.parameters(), lr=0.0005)
 
     # Scoring_function
-    scoring_function = get_scoring_function(scoring_function=scoring_function, num_processes=num_processes,
+    if args.scoring_function == 'activity_model':
+        scoring_function_kwargs = {'clf_path': args.clf_file}
+    
+    scoring_function = get_scoring_function(scoring_function=args.scoring_function, 
+                                            num_processes=num_processes,
                                             **scoring_function_kwargs)
 
     # For policy based RL, we normally train on-policy and correct for the fact that more likely actions
     # occur more often (which means the agent can get biased towards them). Using experience replay is
     # therefor not as theoretically sound as it is for value based RL, but it seems to work well.
     experience = Experience(voc)
-
+    
     # Log some network weights that can be dynamically plotted with the Vizard bokeh app
     logger.log(Agent.rnn.gru_2.weight_ih.cpu().data.numpy()[::100], "init_weight_GRU_layer_2_w_ih")
     logger.log(Agent.rnn.gru_2.weight_hh.cpu().data.numpy()[::100], "init_weight_GRU_layer_2_w_hh")
@@ -82,7 +97,8 @@ def train_agent(restore_prior_from='data/Prior.ckpt',
 
     print("Model initialized, starting training...")
 
-    for step in range(n_steps):
+    sample_every_steps = 50
+    for step in range(args.n_steps):
 
         # Sample from Agent
         seqs, agent_likelihood, entropy = Agent.sample(batch_size)
@@ -135,7 +151,7 @@ def train_agent(restore_prior_from='data/Prior.ckpt',
 
         # Print some information for this step
         time_elapsed = (time.time() - start_time) / 3600
-        time_left = (time_elapsed * ((n_steps - step) / (step + 1)))
+        time_left = (time_elapsed * ((args.n_steps - step) / (step + 1)))
         print("\n       Step {}   Fraction valid SMILES: {:4.1f}  Time elapsed: {:.2f}h Time left: {:.2f}h".format(
               step, fraction_valid_smiles(smiles) * 100, time_elapsed, time_left))
         print("  Agent    Prior   Target   Score             SMILES")
@@ -158,27 +174,27 @@ def train_agent(restore_prior_from='data/Prior.ckpt',
         logger.log("\n".join([smiles + "\t" + str(round(score, 2)) for smiles, score in zip \
                             (smiles[:12], score[:12])]), "SMILES", dtype="text", overwrite=True)
         logger.log(np.array(step_score), "Scores")
-
+        
+        # sample from the model every n steps
+        if step % sample_every_steps == 0:
+            epoch = -1
+            sample_smiles(args.output_dir, args.seed, Agent, 
+                          args.sample_size, epoch, step)
+    
     # If the entire training finishes, we create a new folder where we save this python file
     # as well as some sampled sequences and the contents of the experinence (which are the highest
     # scored sequences seen during training)
-    if not save_dir:
-        save_dir = 'data/results/run_' + time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime())
-    os.makedirs(save_dir)
-    copyfile('train_agent.py', os.path.join(save_dir, "train_agent.py"))
-
-    experience.print_memory(os.path.join(save_dir, "memory"))
-    torch.save(Agent.rnn.state_dict(), os.path.join(save_dir, 'Agent.ckpt'))
+    experience.print_memory(os.path.join(args.output_dir, "memory"))
+    torch.save(Agent.rnn.state_dict(), os.path.join(args.output_dir, 'Agent.ckpt'))
 
     seqs, agent_likelihood, entropy = Agent.sample(256)
     prior_likelihood, _ = Prior.likelihood(Variable(seqs))
     prior_likelihood = prior_likelihood.data.cpu().numpy()
     smiles = seq_to_smiles(seqs, voc)
     score = scoring_function(smiles)
-    with open(os.path.join(save_dir, "sampled"), 'w') as f:
+    with open(os.path.join(args.output_dir, "sampled"), 'w') as f:
         f.write("SMILES Score PriorLogP\n")
         for smiles, score, prior_likelihood in zip(smiles, score, prior_likelihood):
             f.write("{} {:5.2f} {:6.2f}\n".format(smiles, score, prior_likelihood))
 
-if __name__ == "__main__":
-    train_agent()
+train_agent()
